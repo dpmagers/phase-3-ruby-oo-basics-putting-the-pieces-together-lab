bae220d41ba05b0ccaafd3abb303b1cb2c1b8724
Daniel Magers <daniel.magers@gmail.com>
2022-08-31 06:48:28 -0500

    commit tree 0472e36b0315f4d558f175ef5c0d60a78f94e548

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,77 @@
 
+#put in pry
+#create a class
+# with in an initialize method with title as a param
+#put in an accessor reader for the title 
+# add attributes 
+#create turn_page method
+
+
+class Book
+
+    attr_accessor :author, :page_count, :genre
+    attr_reader   :title 
+     
+
+    def initialize(title)
+        @title = title
+        @author = author
+        @page_count = page_count
+        @genre = genre
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+
+end
+
+
+# lib/book.rb
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# require 'pry'
+# class Book
+#     attr_accessor :author, :page_count, :genre
+#     attr_reader :title
+    
+#     def initialize(title)
+#         @title = title   
+#     end
+
+#     def turn_page
+#         puts "Flipping the page...wow, you read fast!"
+#     end
+
+# end
+# binding.pry
+
+
+# lib/book.rb
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,83 @@
-# Make your shoe class here!
+#put in pry
+#create Shoe class 
+#create initialize shoe method with brand as param
+#add attr_reader macro and add brand (pry)
+#add attributes in initialize method (not in param)
+#add attributes to attr_accessor
+#make a cobble method 
+
+require 'pry'
+
+class Shoe
+
+    attr_reader :brand
+    attr_accessor :color, :size, :material, :condition
+
+    def initialize(brand)
+        @brand = brand
+        @color = color
+        @size = size
+        @material = material
+        @condition = condition
+    end
+
+    def cobble
+        puts "Your shoe is as good as new!"
+        @condition = "new"
+    end
+
+
+end
+binding.pry
+
+# lib/shoe.rb
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# require 'pry'
+# class Shoe
+
+#     attr_accessor :color, :size, :material, :condition
+#     attr_reader :brand
+
+#     def initialize(brand)
+#         @brand = brand
+#     end
+
+#     # instance variable, called on specific instance of shoe
+#     # not necessarily every shoe 
+#     def cobble
+#         @condition = "new"
+#         puts "Your shoe is as good as new!"
+
+#     end
+
+# end
+
+# # lib/shoe.rb
+# binding.pry
